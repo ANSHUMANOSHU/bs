3f97958728127b5e964e31129d5aff256c0df26a
Madhur Tripathi <madhur.tripathi001@gmail.com>
2021-05-10 08:36:24 +0530

    Functionality defined for transaction class
    toString, delete, getByAccountNumber, get, save have been defined.

--- src/bs/bean/Transaction.java
+++ src/bs/bean/Transaction.java
@@ -36,18 +36,78 @@ public class Transaction
 
 	public static boolean save(Transaction transaction)
 	{
+		try {
+            
+            String QUERY = " insert into " + TABLE_TRANSACTIONS
+                    + " (" + T_ID + ","
+                    + " " + T_ACCOUNT_NUMBER + ","
+                    + " " + T_DESCRIPTION + ","
+                    + " " + T_TYPE + ","
+                    + " " + T_FINAL_BALANCE + ","
+                    + " " + T_AMOUNT + ","
+                    + " " + T_STAMP + ") "
+                    + " values (UUID(), ?, ?, ?, ?, ?, ?)";
+            
+            Connection connection = Database.getConnection();
+            PreparedStatement preparedStmt = connection.prepareStatement(QUERY);
+            
+            preparedStmt.setString(1, transaction.getAccountNumber());
+            preparedStmt.setString(2, transaction.getDescription());
+            preparedStmt.setString(3, transaction.getType());
+            preparedStmt.setFloat(4, transaction.getFinalBalance());
+            preparedStmt.setFloat(5, transaction.getAmount());
+            preparedStmt.setLong(6, transaction.getStamp());
+            
+            return preparedStmt.execute();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Exception : " + ex);
+        }
 		return false;
 	}
 	
 	public static List<Transaction> get(String QUERY)
 	{
 		List<Transaction> transactions = new ArrayList<>();
+		try {
+            
+            Connection connection = Database.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(QUERY);
+            while (resultSet.next()) {
+                Transaction transaction = new Transaction();
+                transaction.setId(resultSet.getString(resultSet.findColumn(T_ID)));
+                transaction.setAccountNumber(resultSet.getString(resultSet.findColumn(T_ACCOUNT_NUMBER)));
+                transaction.setDescription(resultSet.getString(resultSet.findColumn(T_DESCRIPTION)));
+                transaction.setType(resultSet.getString(resultSet.findColumn(T_TYPE)));
+                transaction.setFinalBalance(resultSet.getFloat(resultSet.findColumn(T_FINAL_BALANCE)));
+                transaction.setAmount(resultSet.getFloat(resultSet.findColumn(T_AMOUNT)));
+                transaction.setStamp(resultSet.getLong(resultSet.findColumn(T_STAMP)));
+                transactions.add(transaction);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Exception : " + ex);
+        }
 		return transactions;
 	}
 	
 	public static void delete(String accountNo)
 	{
+		try {
+            String QUERY = "delete from " + TABLE_TRANSACTIONS + " where " + T_ACCOUNT_NUMBER + " = \"" + accountNumber + "\"";
+            Connection connection = Database.getConnection();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(QUERY);
+        } catch (SQLException ex) {
+            System.out.println("Exception : " + ex);
+        }
 	}
+	
+	public static List<Transaction> getByAccountNumber(String accountNumber) {
+        String QUERY = "select * from " + TABLE_TRANSACTIONS + " where " + T_ACCOUNT_NUMBER + " = \"" + accountNumber + "\" order by " + T_STAMP + " desc";
+        return get(QUERY);
+    }
 
 	public String getType() {
 		return type;
@@ -105,5 +165,7 @@ public class Transaction
 		this.amount = amount;
 	}
 	
-	
+	public String toString() {
+        return "{" + "id=" + id + "\n accountNumber=" + accountNumber + "\n description=" + description + "\n type=" + type + "\n finalBalance=" + finalBalance + "\n amount=" + amount + "\n stamp=" + Utils.toDateTimeString(stamp) + '}';
+    }
 }
