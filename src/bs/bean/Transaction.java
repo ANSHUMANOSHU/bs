b131be83ec9aaba03e369f1bc79572899d4d5319
Madhur Tripathi <madhur.tripathi001@gmail.com>
2021-05-10 08:53:57 +0530

    Fixed Errors in added functionalities
    Fixed Errors from last commit where functionalities were defined.

--- src/bs/bean/Transaction.java
+++ src/bs/bean/Transaction.java
@@ -2,41 +2,52 @@ package bs.bean;
 
 import java.util.ArrayList;
 import java.util.List;
+import bs.db.Database;
+import bs.utils.Utils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Transaction
-{
+
+public class Transaction implements TableDetails {
+
 	/*
+	 * Constants
+	 * */
+    public static final String TYPE_CREDIT = "Credit", TYPE_DEBIT = "Debit";
+
+    /*
 	 * id - Transaction ID
-	 * accountNo - Account Number
+	 * accountNumber - Account Number
 	 * description - Transaction Description
 	 * type - Debit/Credit
 	 * stamp - Time Stamp
 	 * finalBalance - Balance after transaction
 	 * amount - Transaction amount
 	 * */
-	private String id, accountNo, description, type;
-	private float finalBalance, amount;
-	private long stamp;
-	
-	/*
-	 * Constants
-	 * */
-	public static final String TYPE_CREDIT = "Credit", TYPE_DEBIT = "Debit";
-	
-	public Transaction(String id, String accountNo, String description, String type, long stamp, float finalBalance, float amount)
-	{
-		this.id = id;
-		this.accountNo = accountNo;
-		this.description = description;
-		this.type = type;
-		this.stamp = stamp;
-		this.finalBalance = finalBalance;
-		this.amount = amount;
-	}
+    private String id, accountNumber, description, type;
+    private float finalBalance, amount;
+    private long stamp;
+    
+    public Transaction() {
+        
+    }
+    
+    public Transaction(String id, String accountNumber, String description, String type, float finalBalance, float amount, long stamp) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.description = description;
+        this.type = type;
+        this.finalBalance = finalBalance;
+        this.amount = amount;
+        this.stamp = stamp;
+    }
 
-	public static boolean save(Transaction transaction)
-	{
-		try {
+    //STATIC METHODS
+    public static boolean save(Transaction transaction) {
+        try {
             
             String QUERY = " insert into " + TABLE_TRANSACTIONS
                     + " (" + T_ID + ","
@@ -64,13 +75,12 @@ public class Transaction
             ex.printStackTrace();
             System.out.println("Exception : " + ex);
         }
-		return false;
-	}
-	
-	public static List<Transaction> get(String QUERY)
-	{
-		List<Transaction> transactions = new ArrayList<>();
-		try {
+        return false;
+    }
+    
+    public static List<Transaction> get(String QUERY) {
+        List<Transaction> transactions = new ArrayList<>();
+        try {
             
             Connection connection = Database.getConnection();
             Statement statement = connection.createStatement();
@@ -89,12 +99,16 @@ public class Transaction
         } catch (SQLException ex) {
             System.out.println("Exception : " + ex);
         }
-		return transactions;
-	}
-	
-	public static void delete(String accountNo)
-	{
-		try {
+        return transactions;
+    }
+    
+    public static List<Transaction> getByAccountNumber(String accountNumber) {
+        String QUERY = "select * from " + TABLE_TRANSACTIONS + " where " + T_ACCOUNT_NUMBER + " = \"" + accountNumber + "\" order by " + T_STAMP + " desc";
+        return get(QUERY);
+    }
+    
+    public static void delete(String accountNumber) {
+        try {
             String QUERY = "delete from " + TABLE_TRANSACTIONS + " where " + T_ACCOUNT_NUMBER + " = \"" + accountNumber + "\"";
             Connection connection = Database.getConnection();
             Statement statement = connection.createStatement();
@@ -102,70 +116,67 @@ public class Transaction
         } catch (SQLException ex) {
             System.out.println("Exception : " + ex);
         }
-	}
-	
-	public static List<Transaction> getByAccountNumber(String accountNumber) {
-        String QUERY = "select * from " + TABLE_TRANSACTIONS + " where " + T_ACCOUNT_NUMBER + " = \"" + accountNumber + "\" order by " + T_STAMP + " desc";
-        return get(QUERY);
     }
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public float getFinalBalance() {
-		return finalBalance;
-	}
-
-	public void setFinalBalance(float finalBalance) {
-		this.finalBalance = finalBalance;
-	}
-
-	public String getAccountNo() {
-		return accountNo;
-	}
-
-	public void setAccountNo(String accountNo) {
-		this.accountNo = accountNo;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public long getStamp() {
-		return stamp;
-	}
-
-	public void setStamp(long stamp) {
-		this.stamp = stamp;
-	}
-
-	public float getAmount() {
-		return amount;
-	}
-
-	public void setAmount(float amount) {
-		this.amount = amount;
-	}
-	
-	public String toString() {
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public float getFinalBalance() {
+        return finalBalance;
+    }
+    
+    public void setFinalBalance(float finalBalance) {
+        this.finalBalance = finalBalance;
+    }
+    
+    public float getAmount() {
+        return amount;
+    }
+    
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    
+    public long getStamp() {
+        return stamp;
+    }
+    
+    public void setStamp(long stamp) {
+        this.stamp = stamp;
+    }
+    
+    @Override
+    public String toString() {
         return "{" + "id=" + id + "\n accountNumber=" + accountNumber + "\n description=" + description + "\n type=" + type + "\n finalBalance=" + finalBalance + "\n amount=" + amount + "\n stamp=" + Utils.toDateTimeString(stamp) + '}';
     }
+    
 }
